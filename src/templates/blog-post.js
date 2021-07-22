0343c2499f97fd24845818b79754567732bb8c86
Al <webmaster@redakt.eu>
2021-07-22 12:44:17 +0000

    Remove Gutenberg CSS

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,14 +3,6 @@ import { Link, graphql } from "gatsby"
 import Image from "gatsby-image"
 import parse from "html-react-parser"
 
-// We're using Gutenberg so we need the block styles
-// these are copied into this project due to a conflict in the postCSS
-// version used by the Gatsby and @wordpress packages that causes build
-// failures.
-// @todo update this once @wordpress upgrades their postcss version
-import "../css/@wordpress/block-library/build-style/style.css"
-import "../css/@wordpress/block-library/build-style/theme.css"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
